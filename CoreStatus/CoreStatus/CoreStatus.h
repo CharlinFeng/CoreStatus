007be357c799f1d76972ec2079edbc59e8e7436e
Hemy <thislifee@qq.com>
2015-07-07 17:05:13 +0800

    精简文件

--- CoreStatus/CoreStatus/CoreStatus.h
+++ CoreStatus/CoreStatus/CoreStatus.h
@@ -8,8 +8,34 @@
 
 #import <Foundation/Foundation.h>
 #import "Reachability.h"
-#import "CoreNetWorkStatus.h"
 
+/** 网络状态 */
+typedef enum{
+    
+    /** 无网络 */
+    CoreNetWorkStatusNone=0,
+    
+    /** Wifi网络 */
+    CoreNetWorkStatusWifi,
+    
+    /** 蜂窝网络 */
+    CoreNetWorkStatusWWAN,
+    
+    /** 2G网络 */
+    CoreNetWorkStatus2G,
+    
+    /** 3G网络 */
+    CoreNetWorkStatus3G,
+    
+    /** 4G网络 */
+    CoreNetWorkStatus4G,
+    
+    //未知网络
+    CoreNetWorkStatusUnkhow
+    
+}CoreNetWorkStatus;
+
+/** 网络状态改变回调 */
 typedef void (^CSStatusDidChangedBlock) (NSString *statusName,CoreNetWorkStatus status);
 
 @interface CoreStatus : NSObject
